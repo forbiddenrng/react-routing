4362f95f7bfb9583aa0da2ce0b98e5c0dad8b956
Antek <antek.gawron@gmail.com>
2020-09-07 22:23:37 +0200

    Updates

---
+++ precache-manifest.edd22ea78e3647c4294aa2b14ae18610.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "73e4ff6559025a6622d6",
+    "url": "/react-routing/static/css/main.45cc50c5.chunk.css"
+  },
+  {
+    "revision": "73e4ff6559025a6622d6",
+    "url": "/react-routing/static/js/main.e1d2be68.chunk.js"
+  },
+  {
+    "revision": "3a657f46235fb0416efe",
+    "url": "/react-routing/static/js/runtime~main.51ad13a7.js"
+  },
+  {
+    "revision": "23487973340609ec91d2",
+    "url": "/react-routing/static/js/2.88b550cb.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-routing/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-routing/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-routing/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "0c7fd9b10ff8cad8be781df96f5d53f5",
+    "url": "/react-routing/index.html"
+  }
+];
